a5510172a358a9d27c7d2a8e55a55d5ed522ed15
Pronoy Das <pronoysunny.47@gmail.com>
2025-02-18 17:17:53 +0530

    add Loj4j code

--- src/test/java/com/learn/automation/TestRunner/TestRunner.java
+++ src/test/java/com/learn/automation/TestRunner/TestRunner.java
@@ -1,7 +1,10 @@
 package com.learn.automation.TestRunner;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.DataProvider;
-
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.CucumberOptions.SnippetType;
@@ -19,10 +22,20 @@ snippets = SnippetType.CAMELCASE
 )
 public class TestRunner extends AbstractTestNGCucumberTests{
 	
+	public static Logger log = LogManager.getLogger(TestRunner.class);
+	
+	
 	@Override
 	@DataProvider(parallel = false)
 	public Object[][] scenarios(){
 		return super.scenarios();
+	}
+	
+	@BeforeSuite
+	public void setUpLoggert() {
+	 DOMConfigurator.configure(System.getProperty("user.dir")+"\\Resource\\Runner\\log4j.xml");	
+	 log.debug("Log4j Configuration Completed!!");
+	 
 	}
 
 }
