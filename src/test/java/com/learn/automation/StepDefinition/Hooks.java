399d956cb978e2e8e3ab9e63ecb2d2dce8d4b5dd
Pronoy Das <pronoysunny.47@gmail.com>
2025-02-19 20:07:47 +0530

    Add Logger though Hooks

--- src/test/java/com/learn/automation/StepDefinition/Hooks.java
+++ src/test/java/com/learn/automation/StepDefinition/Hooks.java
@@ -2,12 +2,16 @@ package com.learn.automation.StepDefinition;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
+import com.learn.automation.TestRunner.TestRunner;
 import com.learn.automation.Utilities.WebDriverManagerSetup;
 
 import io.cucumber.java.After;
 import io.cucumber.java.AfterAll;
 import io.cucumber.java.AfterStep;
+import io.cucumber.java.Before;
+import io.cucumber.java.BeforeStep;
 import io.cucumber.java.Scenario;
+import io.cucumber.messages.types.StepDefinition;
 
 public class Hooks {
 	
@@ -32,6 +36,24 @@ public class Hooks {
 			TakesScreenshot tss =((TakesScreenshot)WebDriverManagerSetup.driverManager.getWebDriver());
 			byte[] screenshort=tss.getScreenshotAs(OutputType.BYTES);
 			sce.attach(screenshort, "image/png", "image");
+		}
+	}
+	@BeforeStep
+	public void before_Step(Scenario sce) {
+		
+		
+	}
+	
+	@Before
+	public void before_Scenerio(Scenario sce) {
+		TestRunner.log.info(String.format("Start Executing %s Scenario", sce.getName()));
+		
+	}
+	
+	@After
+	public void after_Scenerio(Scenario sce) {
+		if(sce.isFailed()) {
+			TestRunner.log.info(String.format("Scenario Failed !! sratus %s ", sce.getStatus().name()));
 		}
 		
 	}
